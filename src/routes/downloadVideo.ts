3555f28f475a3713c9ee32f8d4dab2f931c089db
DanielBarros854 <daniel@lonewolfcorp.com>
2022-08-03 16:05:36 -0300

    feat: create downloadVideo view

--- src/routes/downloadVideo.ts
+++ src/routes/downloadVideo.ts
@@ -1,7 +1,19 @@
 import { Request, Response, Router } from "express";
+import { join } from "path";
+import { getInfo } from "ytdl-core";
 
 export const downloadVideo = Router();
 
-downloadVideo.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'download' })
+downloadVideo.get('/', async (req: Request, res: Response) => {
+  const video_url = req!.query!.url as string
+  const video_id_formatted = video_url.split('v=')[1]
+
+  const video_info = await getInfo(video_url)
+  const formats_formatted = video_info.formats
+    .sort((a, b) => a.mimeType! < b.mimeType! ? 1 : 0)
+
+  res.render(join(__dirname, '../views/download.ejs'), {
+    url: `https://www.youtube.com/embed/${video_id_formatted}`,
+    info: formats_formatted
+  })
 })
