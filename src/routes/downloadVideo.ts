75922d4228f738f7bb3293ab8070163cab3a8c28
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-08-04 09:08:53 -0300

    feat: create trycath in downloadVideo

--- src/routes/downloadVideo.ts
+++ src/routes/downloadVideo.ts
@@ -5,15 +5,19 @@ import { getInfo } from "ytdl-core";
 export const downloadVideo = Router();
 
 downloadVideo.get('', async (req: Request, res: Response) => {
-  const video_url = req!.query!.url as string
-  const video_id_formatted = video_url.split('v=')[1]
+  try {
+    const video_url = req!.query!.url as string
+    const video_id_formatted = video_url.split('v=')[1]
 
-  const video_info = await getInfo(video_url)
-  const formats_formatted = video_info.formats
-    .sort((a, b) => a.mimeType! < b.mimeType! ? 1 : 0)
+    const video_info = await getInfo(video_url)
+    const formats_formatted = video_info.formats
+      .sort((a, b) => a.mimeType! < b.mimeType! ? 1 : 0)
 
-  res.render(join(__dirname, '../views/download.ejs'), {
-    url: `https://www.youtube.com/embed/${video_id_formatted}`,
-    info: formats_formatted
-  })
+    res.render(join(__dirname, '../views/download.ejs'), {
+      url: `https://www.youtube.com/embed/${video_id_formatted}`,
+      info: formats_formatted
+    })
+  } catch (err) {
+  res.status(500).json(err)
+  }
 })
